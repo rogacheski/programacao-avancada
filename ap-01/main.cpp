40d8358b4a543e148674b0d4d88b0ec5a67a8db8
rogacheski <100004554+rogacheski@users.noreply.github.com>
2022-03-19 23:13:15 -0300

    Update and rename teste to main.cpp

---
+++ ap-01/main.cpp
@@ -0,0 +1,13 @@
+#include "Data.h"
+#include "Bicicleta.h"
+#include "Carro.h"
+#include "Cliente.h"
+#include "AluguelBicicleta.h"
+#include "AluguelCarro.h"
+#include "GerenciadorAluguel.h"
+#include "TipoCombustivel.h"
+
+int main()
+{
+
+};
