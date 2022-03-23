ee3119bc8618a96a6160c0db900ac52903d5f973
rogacheski <100004554+rogacheski@users.noreply.github.com>
2022-03-23 16:48:22 -0300

    Create Conta.h

---
+++ ed-04/Conta.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include "Cliente.h"
+
+class Conta
+{
+private:
+	std::string _numero = "";
+	std::string _agencia = "";
+	Cliente _cliente;
+	float _saldo = 0.0;
+	std::vector<std::string> extrato;
+
+public:
+	Conta() { mostrarConta(); };
+	Conta(std::string numero, std::string agencia, Cliente cliente) : _numero(numero), _agencia(agencia), Cliente(cliente) { mostrarConta(); };
+	void depositar(float valor);
+	void retirar(float valor);
+	void const imprimeExtrato();
+	void alterarCliente(Cliente cliente);
+	void mostrarConta();
+};
