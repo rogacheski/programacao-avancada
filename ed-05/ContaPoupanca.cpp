c8ffd031dfd407aec41825006197e24b3cd18bf8
rogacheski <100004554+rogacheski@users.noreply.github.com>
2022-03-25 16:20:27 -0300

    Create ContaPoupanca.cpp

---
+++ ed-05/ContaPoupanca.cpp
@@ -0,0 +1,12 @@
+void ContaPoupanca::aplicarJurosDiarios(dias, taxa)
+{
+	if (dias == 0)
+	cout << "Juros aplicados" << endl;
+
+	else
+	{
+		_saldo -= _saldo * taxa;
+		dias--;
+		aplicarJurosDiarios(dias, taxa);
+	}
+};
