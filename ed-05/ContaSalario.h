3f3614b246506064bf4b6899b04256448cbdb1ef
rogacheski <100004554+rogacheski@users.noreply.github.com>
2022-03-25 22:22:10 -0300

    Update ContaSalario.h

--- ed-05/ContaSalario.h
+++ ed-05/ContaSalario.h
@@ -4,13 +4,13 @@
 class ContaSalario : public Conta
 {
 private:
-	std::string _numero = "";
-	std::string _agencia = "";
+	std::string _numero;
+	std::string _agencia;
 	Cliente _cliente;
-	float _saldo = 1500.0;
-	std::vector<std::string> extrato;
+	float _saldo;
+	std::vector<std::string> _extrato;
 
 public:
-	ContaSalario()
+	ContaSalario() : _numero(""), _agencia(""), _cliente(Cliente();), _saldo(1500.0);
 	void depositar(float valor) {};
 };
