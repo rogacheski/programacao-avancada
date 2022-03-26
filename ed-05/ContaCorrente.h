173215252659071da575c2e9eaa20876186117f6
rogacheski <100004554+rogacheski@users.noreply.github.com>
2022-03-25 22:25:00 -0300

    Update ContaCorrente.h

--- ed-05/ContaCorrente.h
+++ ed-05/ContaCorrente.h
@@ -5,13 +5,13 @@
 class ContaCorrente : public Conta
 {
 private:
-	std::string _numero = "";
-	std::string _agencia = "";
+	std::string _numero;
+	std::string _agencia;
 	Cliente _cliente;
-	float _saldo = 0.0;
+	float _saldo;
 	std::vector<std::string> extrato;
 
 public:
-	ContaCorrente() : Conta(), Investimento(0.03);
+	ContaCorrente() : Conta(), Investimento(0.03), _numero(""), _agencia(""), _cliente(Cliente();) _saldo(0.0);
 	void aplicarJurosDiarios(dias, taxa);
 };
