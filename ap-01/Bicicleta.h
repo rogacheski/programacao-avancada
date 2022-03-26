d895faba5f7c43381c2695be7a219f94186ab622
rogacheski <100004554+rogacheski@users.noreply.github.com>
2022-03-25 22:11:13 -0300

    Update Bicicleta.h

--- ap-01/Bicicleta.h
+++ ap-01/Bicicleta.h
@@ -5,7 +5,7 @@
 class Bicicleta : public Veiculo
 {
 public:
-	Bicicleta();
+	Bicicleta() : Veiculo();
 	void imprime() const;
 	void aluga() { _disponivel = 0; };
 	void devolve() { _disponivel = 0; };
